047b61ba831a6de788dffedcfa63f1ebb2739bf0
Evgeny Yastremsky <evgeny@cycode.com>
2026-05-14 14:37:21 +0200

    Update API key in main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import secretStore from './secret_store.js';
 
-const apiKey = '12345-ABCDE';
+const apiKey = "wzslrXUtnFEMI/s2MdENG/123456CYEXAMPLEKEY";
 
 function fetchData() {
     const apiUrl = `https://api.example.com/data?api_key=${apiKey}`;
