4ee005f0e26f6194a310dfa505b4fa0005c170b4
Evgeny Yastremsky <evgeny@cycode.com>
2026-05-14 10:05:37 +0200

    Add secret

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const apiKey = '12345-ABCDE';
+const apiSecret = "1a2mzxc90ck434c-34mfdkzs30==";
 
 function fetchData() {
     const apiUrl = `https://api.example.com/data?api_key=${apiKey}`;
@@ -6,6 +7,7 @@ function fetchData() {
         .then(response => response.json())
         .then(data => {
             console.log('Data fetched:', data);
+            console.log('DEBUG LOG:', apiSecret);
         })
         .catch(error => {
             console.error('Error fetching data:', error);
